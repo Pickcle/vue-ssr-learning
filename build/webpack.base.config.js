29d7af171d59186ea62f7d67be67dc59d42b8fb3
Pickcle <348707253@qq.com>
2018-05-14 11:26:35 +0800

    + wip

---
+++ build/webpack.base.config.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+function resolve (dir) {
+  return path.resolve(__dirname, '..', dir)
+}
+
+module.exports = {
+  devtool: isProd ? false : '#cheap-module-source-map',
+  output: {
+    path: resolve('dist'),
+    publicPath: '/dist/',
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    noParse: /es6-promise\.js$/,
+    rules: [
+      {
+        test: /.vue$/,
+        loader: 'vue-loader',
+        options: {
+          compilerOptions: {
+            preserveWhitespace: false
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: isProd
+          ? ExtractTextPlugin.extract({
+              use: [
+                {
+                  loader: 'css-loader',
+                  options: { minimize: true }
+                },
+                'stylus-loader'
+              ],
+              fallback: 'vue-style-loader'
+            })
+          : ['vue-style-loader', 'css-loader', 'stylus-loader']
+      }
+    ]
+  },
+  plugins: isProd
+    ? [
+        new VueLoaderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+          compress: { warnings: false }
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new ExtractTextPlugin({
+          filename: 'common.[chunkhash].css'
+        })
+      ]
+    : [
+        new VueLoaderPlugin(),
+        new FriendlyErrorsPlugin()
+      ]
+}
