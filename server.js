f007fc69ec402c6476374397a2b14b63ff0ac473
Pickcle <348707253@qq.com>
2018-06-12 18:52:53 +0800

    + 为每次请求创建一个新的vue实例

--- server.js
+++ server.js
@@ -3,21 +3,20 @@ const server = require('express')()
 const renderer = require('vue-server-renderer').createRenderer({
   template: require('fs').readFileSync('./src/index.template.html', 'utf-8')
 })
+const createApp = require('./src/app.js')
 
 server.get('*', (req, res) => {
-  const app = new Vue({
-    data: {
-      url: req.url
-    },
-    template: `<div>访问的 URL 是： {{ url }}</div>`
-  })
+  const templateContext = {
+    url: req.url
+  }
+  const app = createApp(templateContext)
 
-  const context = {
+  const htmlContext = {
     title: 'Vue SSR Demo',
     meta: `<meta charset="utf-8">`
   }
 
-  renderer.renderToString(app, context, (err, html) => {
+  renderer.renderToString(app, htmlContext, (err, html) => {
     if (err) {
       res.status(500).end('Internal Server Error')
       return
