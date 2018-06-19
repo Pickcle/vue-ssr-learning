23cd94e54ecb0a3917b8d4f8edad5052ed7f85c2
Pickcle <348707253@qq.com>
2018-06-19 11:40:07 +0800

    + use CommonChunk

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,4 +1,4 @@
-// const webpack = require('webpack')
+const webpack = require('webpack')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.config.js')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
@@ -6,6 +6,22 @@ const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 module.exports = merge(baseConfig, {
   entry: './src/entry-client.js',
   plugins: [
+    // 此插件在输出目录中
+    // 生成 `vue-ssr-client-manifest.json`。
     new VueSSRClientPlugin()
-  ]
+  ],
+  optimization: {
+    runtimeChunk: {
+      name: 'manifest'
+    },
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all"
+        }
+      }
+    }
+  }
 })
